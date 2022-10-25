5c2a7664cb21d0f31bd457ad5cdcb1abd700c7ad
Zsombor Makker <makker.zsombor@gmail.com>
2022-10-25 11:26:01 +0200

    Init commit

---
+++ main.js
@@ -0,0 +1,55 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {    
+
+    // Minden
+    document.getElementById('minden').addEventListener('click', async () => {
+        let response = await fetch('products.json');
+        let eredmeny = await response.json();
+
+        let termekLista = document.getElementById('adatok');
+        termekLista.textContent = '';
+        // ha apple kell akkor appleTermekek a forba
+        for (let p of eredmeny.products){
+            let li = document.createElement('li');
+            li.textContent = p.title + ' leírás: ' + p.description + ' ár: ' + p.price + ' rating: ' + p.rating + ' raktáron: ' + p.stock +
+            ' márka: ' + p.brand + ' kategoria: ' + p.category;
+            termekLista.appendChild(li);
+        }
+    });
+
+    // ABC
+    document.getElementById('abc').addEventListener('click', async () => {
+        let response = await fetch('products.json');
+        let eredmeny = await response.json();
+
+        let abc = eredmeny.products.sort();
+
+        let termekLista = document.getElementById('adatok');
+        termekLista.textContent = '';
+        // ha apple kell akkor appleTermekek a forba
+        for (let p of eredmeny.products){
+            let li = document.createElement('li');
+            li.textContent = p.title;
+            termekLista.appendChild(li);
+        }
+    });
+
+    // Legdrágább
+    document.getElementById('ar').addEventListener('click', () => {
+
+    });
+
+    // Leírásos
+    document.getElementById('kereso').addEventListener('click', () => {
+        let szoveg = document.getElementById('keresett').value;
+    });
+
+    
+    // Ajánlott
+    document.getElementById('100Alatt').addEventListener('click', () => {
+
+    });
+
+});
+
